46f475cf83f2080bec4f5b4a26dff3a25fe469a8
drtnn <8david@inbox.ru>
2021-06-21 18:05:16 +0300

    @FilmCorpBot v0.1

--- logger.py
+++ logger.py
@@ -1,9 +1,12 @@
 import config
 import logging
+import os
 import telebot
 
-file = open(config.LOGFILE, 'a')  # Создание файла, если он отсутствовал
-file.close()
+if os.path.exists(config.LOGFILE):
+    file = open(config.LOGFILE, 'a')  # Создание файла, если он отсутствовал
+    file.close()
+
 logging.basicConfig(filename=config.LOGFILE,
                     format='[%(asctime)s] [%(levelname)s] => %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
